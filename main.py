a5c49a886c65617ebe8c91262b58a34573273d6a
sina1010anis <sina1010anis@gmail.com>
2023-12-03 10:05:25 +0330

    py knapSack

--- main.py
+++ main.py
@@ -170,10 +170,25 @@ print(rabinKarp(text, pattern));
 # import os 
 # os.remove('text.txt')
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+# x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+# y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+# plt.scatter(x, y)
+# plt.show()
 
-x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
-y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+p_item = [0, 2, 3, 5, 4, 1]
+m_item = [0, 3, 4, 8, 5, 6]
+m = 8
+n = len(p_item)
+v = [[0 for x in range(m+1)] for x in range(n+1)]
+for i in range(n+1):
+    for j in range(m+1):
+        if i == 0 or j == 0:
+            v[i][j] = 0
+        elif m_item[i-1] <= j:
+            v[i][j] = max(v[i-1][j], v[i-1][j - m_item[i-1]] + p_item[i-1])
+        else:
+            v[i][j] = v[i-1][j]
 
-plt.scatter(x, y)
-plt.show()
+print(v[n][m])
+
